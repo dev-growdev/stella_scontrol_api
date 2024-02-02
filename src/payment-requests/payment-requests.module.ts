cda6d6658fe36c7c4c99ccbff8cb9c32ace79b2b
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-02 11:13:38 -0300

    refactor: modifica nome do recurso CRUD para payment-requests-general

--- src/payment-requests/payment-requests.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PaymentRequestsService } from './payment-requests.service';
-import { PaymentRequestsController } from './payment-requests.controller';
-
-@Module({
-  controllers: [PaymentRequestsController],
-  providers: [PaymentRequestsService]
-})
-export class PaymentRequestsModule {}
