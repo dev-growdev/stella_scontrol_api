be0e273068e15b8753f56fec3c546a4002d430b4
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-07 10:33:10 -0300

    refactor: modifica nome da constante

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,40 +1,43 @@
-import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDTO } from './dtos';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService, private jwt: JwtService) { }
+  constructor(private prisma: PrismaService, private jwt: JwtService) {}
 
   async signin(dto: AuthDTO) {
     try {
       const existingUser = await this.prisma.user.findUnique({
         where: {
-          email: dto.email
-        }
+          email: dto.email,
+        },
       });
 
       if (!existingUser) {
-
         const createdUser = await this.prisma.user.create({
           data: {
             idUserAd: dto.idUserAd,
             email: dto.email,
-            name: dto.name
-          }
+            name: dto.name,
+          },
         });
 
         const access_token = await this.jwt.signAsync({
           uid: createdUser.uid,
           name: createdUser.name,
           email: createdUser.email,
-          idUserAd: createdUser.idUserAd
+          idUserAd: createdUser.idUserAd,
         });
 
         return {
           user: createdUser,
-          access_token
+          access_token,
         };
       }
 
@@ -42,34 +45,33 @@ export class AuthService {
         uid: existingUser.uid,
         name: existingUser.name,
         email: existingUser.email,
-        idUserAd: existingUser.idUserAd
+        idUserAd: existingUser.idUserAd,
       });
 
       return {
         user: existingUser,
-        access_token
+        access_token,
       };
-
     } catch (error) {
       throw new InternalServerErrorException(error.message);
     }
   }
 
   async getUserByUid(userId: string) {
-    const userDB = await this.prisma.user.findUnique({
-      where: { uid: userId }
+    const foundUser = await this.prisma.user.findUnique({
+      where: { uid: userId },
     });
 
-    if (!userDB) {
+    if (!foundUser) {
       throw new UnauthorizedException('Usuário não autorizado.');
     }
 
     const user: any = {
-      uuid: userDB.uid,
-      idUserAd: userDB.idUserAd,
+      uuid: foundUser.uid,
+      idUserAd: foundUser.idUserAd,
       data: {
-        displayName: userDB.name,
-        email: userDB.email,
+        displayName: foundUser.name,
+        email: foundUser.email,
         photoURL: '',
         shortcuts: [],
       },
