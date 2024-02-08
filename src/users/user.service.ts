d6c7cc37932bc993dc5cc1ccc27d7d8a6c333b12
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-08 08:19:22 -0300

    feat: adiciona remove, no service

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
+
+  async remove(idUserAd: string) {
+    try {
+      const deletedUser = await this.prisma.user.delete({
+        where: {
+          idUserAd: idUserAd,
+        },
+      });
+
+      if (!deletedUser) {
+        throw new NotFoundException(`Usuário não encontrado!`);
+      }
+
+      return deletedUser;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
 }
