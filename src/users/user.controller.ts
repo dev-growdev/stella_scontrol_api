0bd8b6ff49e5c53a7e287b881e43595d82c0c187
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-08 08:18:36 -0300

    feat: adiciona rota de deletar usuário

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly usersService: UserService) {}
+
+  @Delete(':id_user_ad')
+  remove(@Param('id_user_ad') id_user_ad: string) {
+    return this.usersService.remove(id_user_ad);
+  }
 }
