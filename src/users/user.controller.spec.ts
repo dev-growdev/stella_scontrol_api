ededc069b9448cd7c87a85ea0876178ae7d6b6f4
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-08 08:50:49 -0300

    feat: insere testes da rota

--- src/users/user.controller.spec.ts
+++ src/users/user.controller.spec.ts
@@ -1,20 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { BadRequestException, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
+
+const mockIdUserAd = 'mockUserIdAd';
+const mockDeletedUser = {
+  uid: 'mockUserId',
+  name: 'Mock User',
+  email: 'mockuser@example.com',
+  idUserAd: 'mockUserIdAd',
+};
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [UserService, PrismaService, ConfigService],
     }).compile();
-
+  
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('Remove - DELETE', () => {
+    it('should call userService.remove with the provided id_user_ad and return the deleted user', async () => {
+      jest.spyOn(service, 'remove').mockResolvedValue(mockDeletedUser);
+
+      const result = await controller.remove(mockIdUserAd);
+
+      expect(service.remove).toHaveBeenCalledWith(mockIdUserAd);
+      expect(result).toEqual(mockDeletedUser);
+    });
+
+    it('should handle BadRequestException when id_user_ad is not provided', async () => {
+      jest.spyOn(service, 'remove').mockImplementation(() => {
+        throw new BadRequestException('ID do usuário não fornecido');
+      });
+
+      try {
+        await controller.remove('');
+      } catch (error) {
+        expect(error.message).toBe('ID do usuário não fornecido');
+      }
+    });
+
+    it('should handle NotFoundException when the user with the provided id_user_ad is not found', async () => {
+      jest.spyOn(service, 'remove').mockImplementation(() => {
+        throw new NotFoundException('Usuário não encontrado!');
+      });
+
+      try {
+        await controller.remove(mockIdUserAd);
+      } catch (error) {
+        expect(error.message).toBe('Usuário não encontrado!');
+      }
+    });
+
+    it('should handle InternalServerErrorException for other unexpected errors', async () => {
+      jest.spyOn(service, 'remove').mockImplementation(() => {
+        throw new InternalServerErrorException('Some error');
+      });
+
+      try {
+        await controller.remove(mockIdUserAd);
+      } catch (error) {
+        expect(error.message).toBe('Some error');
+      }
+    });
   });
 });
--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -1,18 +1,87 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PrismaService } from '../prisma/prisma.service';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 
-describe('UserService', () => {
-  let service: UserService;
+const mockPrismaService = {
+  user: {
+    delete: jest.fn(),
+  },
+};
+
+describe('UserController', () => {
+  let controller: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      controllers: [UserController],
+      providers: [
+        UserService,
+        { provide: PrismaService, useValue: mockPrismaService },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(controller).toBeDefined();
+  });
+
+  describe('remove', () => {
+    const deleteSpy = jest.spyOn(mockPrismaService.user, 'delete');
+
+    afterEach(() => {
+      deleteSpy.mockClear();
+    });
+
+    it('should remove a user by id_user_ad', async () => {
+      const idUserAd = 'someUserIdAd';
+      const deletedUser = { /* Mock your deleted user data here */ };
+
+      deleteSpy.mockResolvedValue(deletedUser);
+
+      const result = await controller.remove(idUserAd);
+
+      expect(deleteSpy).toHaveBeenCalledWith({ where: { idUserAd } });
+      expect(result).toEqual(deletedUser);
+    });
+
+    it('should handle BadRequestException when id_user_ad is not provided', async () => {
+      const idUserAd = ''; // Empty id_user_ad
+
+      await expect(controller.remove(idUserAd)).rejects.toThrowError(
+        new BadRequestException('ID do usuário não fornecido'),
+      );
+    });
+
+    it('should handle NotFoundException when user is not found', async () => {
+      const idUserAd = 'nonExistentUserIdAd';
+
+      deleteSpy.mockResolvedValue(null);
+
+      await expect(controller.remove(idUserAd)).rejects.toThrowError(
+        new NotFoundException('Usuário não encontrado!'),
+      );
+    });
+
+    it('should handle InternalServerErrorException on unexpected errors', async () => {
+      const idUserAd = 'someUserIdAd';
+
+      deleteSpy.mockImplementation(() => {
+        throw new Error('Some unexpected error');
+      });
+
+      await expect(controller.remove(idUserAd)).rejects.toThrowError(
+        new InternalServerErrorException('Some unexpected error'),
+      );
+    });
   });
 });
