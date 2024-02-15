812bf005a870c93a3a2a8795b8f92a7604df0d7d
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-15 12:14:37 -0300

    refactor: refatora testes de acordo com as rotas

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -13,7 +13,7 @@ import { ConfigService } from '@nestjs/config';
 const makeCategoriesDTO = (): CategoriesDTO => {
   return {
     name: 'Eletrônicos',
-    enable: true
+    enable: true,
   };
 };
 
@@ -89,8 +89,16 @@ describe('CategoriesController', () => {
   describe('FindAll - GET', () => {
     it('should get all categories on success', async () => {
       const categoriesData = [
-        { uid: 'fcca0cf5-4e29-41e1-83ec-ce14fa973b11', name: 'Eletrônicos', enable: true },
-        { uid: '4f8f05a2-c694-4e7f-b972-4bad692e0ca5', name: 'Moda', enable: true },
+        {
+          uid: 'fcca0cf5-4e29-41e1-83ec-ce14fa973b11',
+          name: 'Eletrônicos',
+          enable: true,
+        },
+        {
+          uid: '4f8f05a2-c694-4e7f-b972-4bad692e0ca5',
+          name: 'Moda',
+          enable: true,
+        },
       ];
 
       jest.spyOn(service, 'findAll').mockImplementation(async () => {
@@ -129,7 +137,10 @@ describe('CategoriesController', () => {
         });
       });
 
-      const result = await controller.update(uid, categoryDTO);
+      const result = await controller.update(
+        'fcca0cf5-4e29-41e1-83ec-ce14fa973b11',
+        categoryDTO,
+      );
 
       expect(result).toBeDefined();
       expect(result.uid).toEqual('fcca0cf5-4e29-41e1-83ec-ce14fa973b11');
@@ -144,7 +155,7 @@ describe('CategoriesController', () => {
       });
 
       try {
-        await controller.update(uid, categoryDTO);
+        await controller.update('nonexistent-uid', categoryDTO);
       } catch (error) {
         expect(error).toBeInstanceOf(NotFoundException);
         expect(error.message).toEqual('Categoria não encontrada.');
@@ -159,7 +170,66 @@ describe('CategoriesController', () => {
       });
 
       try {
-        await controller.update(uid, categoryDTO);
+        await controller.update(
+          'fcca0cf5-4e29-41e1-83ec-ce14fa973b11',
+          categoryDTO,
+        );
+      } catch (error) {
+        expect(error).toBeInstanceOf(InternalServerErrorException);
+        expect(error.message).toEqual('Test error');
+      }
+    });
+  });
+
+  describe('Disable - PUT', () => {
+    it('should disable a category and return the updated data on success', async () => {
+      const categoryDTO = { name: 'Eletrônicos', enable: false };
+
+      jest.spyOn(service, 'disable').mockImplementation(async () => {
+        return Promise.resolve({
+          uid: 'fcca0cf5-4e29-41e1-83ec-ce14fa973b11',
+          name: 'Eletrônicos',
+          enable: false,
+        });
+      });
+
+      const result = await controller.dissable(
+        'fcca0cf5-4e29-41e1-83ec-ce14fa973b11',
+        categoryDTO,
+      );
+
+      expect(result).toBeDefined();
+      expect(result.uid).toEqual('fcca0cf5-4e29-41e1-83ec-ce14fa973b11');
+      expect(result.enable).toEqual(false);
+    });
+
+    it('should handle not found category and return NotFoundException on failure', async () => {
+      const categoryDTO = { name: 'Eletrônicos', enable: false };
+
+      jest.spyOn(service, 'disable').mockImplementation(async () => {
+        throw new NotFoundException('Categoria não encontrada.');
+      });
+
+      try {
+        await controller.dissable('nonexistent-uid', categoryDTO);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual('Categoria não encontrada.');
+      }
+    });
+
+    it('should handle errors and return InternalServerErrorException on failure', async () => {
+      const categoryDTO = { name: 'Eletrônicos', enable: false };
+
+      jest.spyOn(service, 'disable').mockImplementation(async () => {
+        throw new InternalServerErrorException('Test error');
+      });
+
+      try {
+        await controller.dissable(
+          'fcca0cf5-4e29-41e1-83ec-ce14fa973b11',
+          categoryDTO,
+        );
       } catch (error) {
         expect(error).toBeInstanceOf(InternalServerErrorException);
         expect(error.message).toEqual('Test error');
