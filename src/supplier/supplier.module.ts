0a2678cb1d101a333f7f07f0296119d782a9b77c
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-22 12:17:40 -0300

    refactor: remove SiegerRepository do providers e exports

--- src/supplier/supplier.module.ts
+++ src/supplier/supplier.module.ts
@@ -6,7 +6,5 @@ import { IntegrationsModule } from 'src/integrations/integration.module';
 @Module({
   imports: [IntegrationsModule],
   controllers: [SupplierController],
-  providers: [SiegerRepository],
-  exports: [SiegerRepository],
 })
 export class SupplierModule {}
