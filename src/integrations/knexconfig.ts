16c50838656f2afabf0963665a0cd1674ac9051e
Carla Silva <carla.silva@movestock.com>
2024-02-22 10:58:28 -0300

    refactor: added database infos

--- src/integrations/knexconfig.ts
+++ src/integrations/knexconfig.ts
@@ -3,9 +3,13 @@ import { Knex } from 'knex';
 export const knexConfig: Knex.Config = {
   client: 'mysql',
   connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'stella',
+    // host: process.env.DB_HOST_SIEGER,
+    // user: process.env.DB_USER_SIEGER,
+    // password: process.env.DB_PASSWORD_SIEGER,
+    // database: process.env.DB_DATABASE_SIEGER,
+    host: 'srvdados01.stelladesign.local',
+    user: 'growdev',
+    password: 'growdev@stella',
+    database: '02970s001',
   },
 };
--- src/integrations/sieger.repository.ts
+++ src/integrations/sieger.repository.ts
@@ -9,21 +9,19 @@ export class SiegerRepository {
   constructor(private readonly knex: Knex) { }
 
   async findSupplierByCNPJ(cnpj: string): Promise<SupplierSieger | undefined> {
-    const result = await this.knex('fcadas')
+    const result = await this.knex('02970s001.cadastro_fornecedor')
       .select([
-        'fcad cgc as cnpj',
-        'fcad des as name',
-        'fcad fan as trade_name',
+        'CNPJ_CPF as cnpj',
+        'Razao_social as name',
+        'Nome_fantasia as trade_name',
       ])
-      .where('fcad tip', 'F')
-      .whereIn('fcad emp', ['SLA', 'SLL', 'STE'])
-      .andWhere('fcad cgc', cnpj);
+      .where('CNPJ_CPF', cnpj);
 
     if (result.length === 0) return undefined;
 
     return {
       cnpj: result[0].cnpj,
-      name: result[0].company_name,
+      name: result[0].name,
       tradeName: result[0].trade_name,
     };
   }
