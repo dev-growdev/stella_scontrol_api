1efe32dd62ff8254bead5d893f276939c0ea7403
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-23 16:26:14 -0300

    refactor: remove trade_name e modifica nome para cpf_cnpj

--- src/integrations/sieger.repository.ts
+++ src/integrations/sieger.repository.ts
@@ -7,23 +7,20 @@ import { InjectConnection } from 'nestjs-knex';
 
 @Injectable()
 export class SiegerRepository {
-  constructor(@InjectConnection() private readonly knex: Knex) { }
+  constructor(@InjectConnection() private readonly knex: Knex) {}
 
-  async findSupplierByCNPJ(cnpj: string): Promise<SupplierSieger | undefined> {
+  async findSupplierByCPForCNPJ(
+    cpfCnpj: string,
+  ): Promise<SupplierSieger | undefined> {
     const result = await this.knex('02970s001.cadastro_fornecedor')
-      .select([
-        'CNPJ_CPF as cnpj',
-        'Razao_social as name',
-        'Nome_fantasia as trade_name',
-      ])
-      .where('CNPJ_CPF', cnpj);
+      .select(['CNPJ_CPF as cpf_cnpj', 'Razao_social as name'])
+      .where('CNPJ_CPF', cpfCnpj);
 
     if (result.length === 0) return undefined;
 
     return {
-      cnpj: result[0].cnpj,
+      cpfCnpj: result[0].cpf_cnpj,
       name: result[0].name,
-      tradeName: result[0].trade_name,
     };
   }
 }
