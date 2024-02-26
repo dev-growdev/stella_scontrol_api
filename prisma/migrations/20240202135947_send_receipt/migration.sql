8f5e2d4a8887101332ad8e2729202089b6622744
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-26 12:04:33 -0300

    refactor: remove migrations

--- prisma/migrations/20240202135947_send_receipt/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `required_receipt` on the `payment_requests_general` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "payment_requests_general" DROP COLUMN "required_receipt",
-ADD COLUMN     "send_receipt" BOOLEAN NOT NULL DEFAULT false;
