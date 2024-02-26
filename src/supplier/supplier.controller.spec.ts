8c95fb07e9fa763630f59c8faac1b9771aa5e728
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-26 12:08:34 -0300

    feat: adiciona testes do supplier

--- src/supplier/supplier.controller.spec.ts
+++ src/supplier/supplier.controller.spec.ts
@@ -1,18 +1,88 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SupplierController } from './supplier.controller';
+import { SupplierService } from './supplier.service';
+import { ReceitawsRepository } from '../integrations/receitaws.repository';
+import { SiegerRepository } from '../integrations/sieger.repository';
+import { InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
+import { KnexModule } from 'nestjs-knex';
+import { knexConfig } from '../integrations/knexconfig';
 
 describe('SupplierController', () => {
   let controller: SupplierController;
+  let service: SupplierService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SupplierController],
+      providers: [
+        SupplierService,
+        ReceitawsRepository,
+        SiegerRepository,
+        PrismaService,
+        ConfigService,
+      ],
+      imports: [
+        KnexModule.forRoot({
+          config: knexConfig,
+        }),
+      ],
     }).compile();
 
     controller = module.get<SupplierController>(SupplierController);
+    service = module.get<SupplierService>(SupplierService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should find supplier by CPF or CNPJ successfully from SiegerRepository', async () => {
+    const cpfOrCnpj = '1234567891011';
+    const expectedResult = {
+      cpfCnpj: '1234567891011',
+      name: 'EMPRESA LTDA',
+    };
+
+    jest
+      .spyOn(service['siegerRepository'], 'findSupplierByCPForCNPJ')
+      .mockResolvedValue(expectedResult);
+
+    const result = await controller.findSupplierByCPForCNPJ(cpfOrCnpj);
+
+    expect(result).toBe(expectedResult);
+  });
+
+  it('should find supplier by CPF or CNPJ successfully from ReceitawsRepository', async () => {
+    const cpfOrCnpj = '1234567891011';
+    const expectedResult = {
+      uid: '1bba2a6b-dbd1-442c-aae3-8f080f441d7d',
+      name: 'EMPRESA LTDA',
+      cnpj: '1234567891011',
+      source: 'receita',
+    };
+
+    jest
+      .spyOn(service['receitawsRepository'], 'findSupplierByCNPJ')
+      .mockResolvedValue(expectedResult);
+
+    const result = await controller.findSupplierByCPForCNPJ(cpfOrCnpj);
+    expect(result).toStrictEqual(expectedResult);
+  });
+
+  it('should handle errors when finding supplier by CPF or CNPJ', async () => {
+    const cpfOrCnpj = 'invalid-value';
+
+    jest
+      .spyOn(service['siegerRepository'], 'findSupplierByCPForCNPJ')
+      .mockRejectedValue(new InternalServerErrorException('Error message'));
+    jest
+      .spyOn(service['receitawsRepository'], 'findSupplierByCNPJ')
+      .mockRejectedValue(new InternalServerErrorException('Error message'));
+
+    await expect(controller.findSupplierByCPForCNPJ(cpfOrCnpj)).rejects.toThrow(
+      InternalServerErrorException,
+    );
   });
 });
--- src/supplier/supplier.service.spec.ts
+++ src/supplier/supplier.service.spec.ts
@@ -1,18 +1,95 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SupplierService } from './supplier.service';
+import { ReceitawsRepository } from '../integrations/receitaws.repository';
+import { SiegerRepository } from '../integrations/sieger.repository';
+import { InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { KnexModule } from 'nestjs-knex';
+import { knexConfig } from '../integrations/knexconfig';
+import { ConfigService } from '@nestjs/config';
+import { truncatePrisma } from 'test/setup/truncate-database';
+import prisma from 'test/setup/client';
 
 describe('SupplierService', () => {
   let service: SupplierService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SupplierService],
+      providers: [
+        SupplierService,
+        PrismaService,
+        SiegerRepository,
+        ReceitawsRepository,
+        ConfigService,
+      ],
+      imports: [
+        KnexModule.forRoot({
+          config: knexConfig,
+        }),
+      ],
     }).compile();
 
     service = module.get<SupplierService>(SupplierService);
   });
 
+  afterAll(async () => {
+    await truncatePrisma();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('FindSupplierByCPForCNPJ - POST', () => {
+    it('should find supplier by CPF or CNPJ successfully from SiegerRepository', async () => {
+      const cpfOrCnpj = '1234567891011';
+      const expectedResult = {
+        cpfCnpj: '1234567891011',
+        name: 'EMPRESA LTDA',
+      };
+
+      jest
+        .spyOn(service['siegerRepository'], 'findSupplierByCPForCNPJ')
+        .mockResolvedValue(expectedResult);
+
+      const result = await service.findSupplierByCPForCNPJ(cpfOrCnpj);
+
+      expect(result).toBe(expectedResult);
+    });
+
+    it('should find supplier by CPF or CNPJ successfully from ReceitawsRepository', async () => {
+      const cpfOrCnpj = '1234567891011';
+      const expectedResult = {
+        uid: 'b68a6a3c-635f-4c06-880c-93c2a2f3c83e',
+        name: 'EMPRESA LTDA',
+        cnpj: '1234567891011',
+        source: 'receita',
+      };
+
+      jest
+        .spyOn(service['receitawsRepository'], 'findSupplierByCNPJ')
+        .mockResolvedValue(expectedResult);
+
+      const result = await service.findSupplierByCPForCNPJ(cpfOrCnpj);
+
+      expect(result).toHaveProperty('name', expectedResult.name);
+      expect(result).toHaveProperty('cnpj', expectedResult.cnpj);
+      expect(result).toHaveProperty('source', expectedResult.source);
+    });
+
+    it('should handle errors when finding supplier by CPF or CNPJ', async () => {
+      const cpfOrCnpj = '00000000000000';
+
+      jest
+        .spyOn(service['siegerRepository'], 'findSupplierByCPForCNPJ')
+        .mockRejectedValue(new InternalServerErrorException('Error message'));
+      jest
+        .spyOn(service['receitawsRepository'], 'findSupplierByCNPJ')
+        .mockRejectedValue(new InternalServerErrorException('Error message'));
+
+      await expect(service.findSupplierByCPForCNPJ(cpfOrCnpj)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+    });
   });
 });
