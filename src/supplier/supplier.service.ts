3525b5df3e107a68cb8642386aee660c618aa837
Marcele Cardozo <marcelecardozo@gmail.com>
2024-02-28 11:54:36 -0300

    refactor: refatora a lógica

--- src/supplier/supplier.service.ts
+++ src/supplier/supplier.service.ts
@@ -2,6 +2,7 @@ import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ReceitawsRepository } from '../integrations/receitaws.repository';
 import { SiegerRepository } from '../integrations/sieger.repository';
 import { PrismaService } from '../prisma/prisma.service';
+import { SupplierDto } from './dto/suppplier.dto';
 
 @Injectable()
 export class SupplierService {
@@ -11,53 +12,69 @@ export class SupplierService {
     private receitawsRepository: ReceitawsRepository,
   ) {}
 
-  async findSupplierByCPForCNPJ(cpfOrCnpj: string) {
+  async findSupplierByCPForCNPJ(
+    cpfOrCnpj: string,
+  ): Promise<SupplierDto | undefined> {
     try {
-      const supplierSieger =
-        await this.siegerRepository.findSupplierByCPForCNPJ(cpfOrCnpj);
+      const supplierSiger = await this.siegerRepository.findSupplierByCPForCNPJ(
+        cpfOrCnpj,
+      );
 
-      if (!supplierSieger) {
-        const existingSupplier = await this.prisma.tempSuppliersData.findUnique(
-          {
-            where: { cnpj: cpfOrCnpj },
-            select: {
-              uid: true,
-              name: true,
-              cnpj: true,
-              source: true,
-            },
-          },
-        );
-
-        if (!existingSupplier) {
-          const supplierReceita =
-            await this.receitawsRepository.findSupplierByCNPJ(cpfOrCnpj);
-
-          if (supplierReceita) {
-            const formatCnpj = cpfOrCnpj.replace(/\D/g, '');
-
-            const createdSupplier = await this.prisma.tempSuppliersData.create({
-              data: {
-                name: supplierReceita.name,
-                cnpj: formatCnpj,
-                source: 'receita',
-              },
-              select: {
-                uid: true,
-                name: true,
-                cnpj: true,
-                source: true,
-              },
-            });
-
-            return createdSupplier;
-          }
-        }
-
-        return existingSupplier;
+      if (supplierSiger) {
+        return {
+          cpfOrCpnj: supplierSiger.cpfCnpj,
+          name: supplierSiger.name,
+          source: 'siger',
+        };
       }
 
-      return supplierSieger;
+      const existingSupplier = await this.prisma.tempSuppliersData.findUnique({
+        where: { cnpj: cpfOrCnpj },
+        select: {
+          uid: true,
+          name: true,
+          cnpj: true,
+          source: true,
+        },
+      });
+
+      if (existingSupplier) {
+        return {
+          cpfOrCpnj: existingSupplier.cnpj,
+          name: existingSupplier.name,
+          source: existingSupplier.source,
+          uid: existingSupplier.uid,
+        };
+      }
+
+      const supplierReceita = await this.receitawsRepository.findSupplierByCNPJ(
+        cpfOrCnpj,
+      );
+
+      if (!supplierReceita) return undefined;
+
+      const formatCnpj = cpfOrCnpj.replace(/\D/g, '');
+
+      const createdSupplier = await this.prisma.tempSuppliersData.create({
+        data: {
+          name: supplierReceita.name,
+          cnpj: formatCnpj,
+          source: 'receita',
+        },
+        select: {
+          uid: true,
+          name: true,
+          cnpj: true,
+          source: true,
+        },
+      });
+
+      return {
+        cpfOrCpnj: createdSupplier.cnpj,
+        name: createdSupplier.name,
+        source: createdSupplier.source,
+        uid: createdSupplier.uid,
+      };
     } catch (error) {
       throw new InternalServerErrorException(error.message);
     }
