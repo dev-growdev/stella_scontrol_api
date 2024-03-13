6c1230171816d161bbcdda6e1b9f0d05bdbce773
Marcele Cardozo <marcelecardozo@gmail.com>
2024-03-13 10:24:10 -0300

    refactor: adiciona valor total e conta contábil

--- src/payment-requests-general/payment-requests-general.service.ts
+++ src/payment-requests-general/payment-requests-general.service.ts
@@ -68,6 +68,8 @@ export class PaymentRequestsGeneralService {
             description: true,
             supplier: true,
             requiredReceipt: true,
+            totalValue: true,
+            accountingAccount: true,
           },
         });
 
