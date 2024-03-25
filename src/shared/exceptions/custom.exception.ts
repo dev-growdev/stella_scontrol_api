fb0c0f2d7fefee575a038927902c5232d4bb8d32
Diener Dornelas <diener.ld@outlook.com>
2024-03-25 21:17:57 +0000

    refactor: adjust custom response exception

--- src/shared/exceptions/custom.exception.ts
+++ src/shared/exceptions/custom.exception.ts
@@ -1,11 +1,12 @@
 import {
+  ArgumentsHost,
   Catch,
   ExceptionFilter,
-  ArgumentsHost,
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { Request, Response } from 'express';
 
 /**
  * Documentação sobre este padrão de exceção: https://docs.nestjs.com/exception-filters#catch-everything
@@ -20,20 +21,24 @@ export class CustomExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
 
     const statusCode =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
+    let message =
+      exception.message || 'Erro interno. Tente novamente mais tarde.';
 
-    const message =
-      exception.message ||
-      'Erro interno do servidor. Tente novamente mais tarde.';
+    if (exception instanceof PrismaClientKnownRequestError) {
+      message = `Erro interno. Tente novamente mais tarde. - Error: ${exception.code}`;
+    }
 
     response.status(statusCode).json({
-      success: false,
       code: statusCode,
+      success: false,
       message,
+      invalidFields: exception.response?.invalidFields,
       stack: exception.stack,
     });
   }
