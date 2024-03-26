9e57a7a934ec482419a244c1698a23b7c6737d86
Carla <carlarhentschel@gmail.com>
2024-03-26 14:59:24 -0300

    refactor: fix invalid email message

--- src/modules/s-quality/supplier/dto/supplier-input.dto.ts
+++ src/modules/s-quality/supplier/dto/supplier-input.dto.ts
@@ -1,46 +1,52 @@
-import { Transform, Type } from "class-transformer";
-import { IsBoolean, IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { Transform, Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateSupplierDto {
-    @IsString({message: "Deve ser uma string"})
-    @IsNotEmpty({message: "Campo obrigatório."})
-    name: string;
+  @IsString({ message: 'Deve ser uma string' })
+  @IsNotEmpty({ message: 'Campo obrigatório.' })
+  name: string;
 
-    @IsString({message: "Deve ser uma string"})
-    @IsNotEmpty({message: "Campo obrigatório."})
-    @IsEmail({}, {message: "E-mail válido."})
-    email: string;
+  @IsString({ message: 'Deve ser uma string' })
+  @IsNotEmpty({ message: 'Campo obrigatório.' })
+  @IsEmail({}, { message: 'E-mail inválido.' })
+  email: string;
 
-    @IsString({message: "Deve ser uma string"})
-    @IsNotEmpty({message: "Campo obrigatório."})
-    continent: string;
+  @IsString({ message: 'Deve ser uma string' })
+  @IsNotEmpty({ message: 'Campo obrigatório.' })
+  continent: string;
 
-    @IsString({message: "Deve ser uma string"})
-    @IsOptional()
-    phoneNumber?: string;
+  @IsString({ message: 'Deve ser uma string' })
+  @IsOptional()
+  phoneNumber?: string;
 
-    @IsString({message: "Deve ser uma string"})
-    @IsOptional()
-    address?: string;
+  @IsString({ message: 'Deve ser uma string' })
+  @IsOptional()
+  address?: string;
 
-    @IsString({message: "Deve ser uma string"})
-    @IsOptional()
-    city?: string;
+  @IsString({ message: 'Deve ser uma string' })
+  @IsOptional()
+  city?: string;
 
-    @IsString({message: "Deve ser uma string"})
-    @IsOptional()
-    region?: string;
+  @IsString({ message: 'Deve ser uma string' })
+  @IsOptional()
+  region?: string;
 
-    @IsString({message: "Deve ser uma string"})
-    @IsOptional()
-    country?: string;
+  @IsString({ message: 'Deve ser uma string' })
+  @IsOptional()
+  country?: string;
 
-    @IsString({message: "Deve ser uma string"})
-    @IsOptional()
-    contactName?: string;
+  @IsString({ message: 'Deve ser uma string' })
+  @IsOptional()
+  contactName?: string;
 
-    @IsBoolean()
-    /* @Type(() => Boolean) */
-    /* @Transform(({value}) => value === "true") */
-    enable: boolean;
+  @IsBoolean()
+  /* @Type(() => Boolean) */
+  /* @Transform(({value}) => value === "true") */
+  enable: boolean;
 }
